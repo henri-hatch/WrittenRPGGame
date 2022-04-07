d18d68f7f5b5eede17ea8e8e97d3540e65ef3baa
henrihatch <henrihatch@gmail.com>
2022-04-07 01:06:33 -0500

    Added Player.cs class to create the player and their properties.

---
+++ WrittenRPGGame/Player.cs
@@ -0,0 +1,14 @@
+namespace WrittenRPGGame;
+
+public class Player
+{
+    
+    public int CurrentHp { get; set; }
+    public int MaxHp { get; set; }
+    public int Gold { get; set; }
+    public int Exp { get; set; }
+    public int Level { get; set; }
+    public int Strength { get; set; }
+    public int Armor { get; set; }
+    public int Intelligence { get; set; }
+}
